fc7c35bf325f8ad02e010999484cf3b7de1f5296
kohei <koufuku69@gmail.com>
2019-11-14 14:34:02 +0900

    プロジェクトとリザルト画像追加しました

---
+++ 1_trail_renderer/PW4-1113-1/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3.0f;
+    GameObject sword;
+    void Start()
+    {
+        sword = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+
+        }
+        if (Input.GetKey("down"))
+        {
+
+        }
+        if (Input.GetKey("right"))
+        {
+
+        }
+        if (Input.GetKey("left"))
+        {
+
+        }
+
+        Swipe();
+    }
+
+    void Swipe()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 target_pos = ray.GetPoint(5.0f);
+
+            sword.transform.LookAt(target_pos);
+        }
+    }
+}
